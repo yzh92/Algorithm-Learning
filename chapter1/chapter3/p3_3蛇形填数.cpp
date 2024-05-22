3eaf584ad3efa5c768f5959af0a3ee0b35ea1102
yzh92 <2426555967@qq.com>
2024-05-22 21:25:15 +0800

    part of chapter3

--- chapter1/chapter3/p3_3蛇形填数.cpp
+++ chapter1/chapter3/p3_3蛇形填数.cpp
@@ -1,25 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<string.h>
-#define maxn 20
+#define maxn 25
+
 int a[maxn][maxn];
 
 int main() {
-	int n, x, y, tot=0;
-	scanf("%d", &n);
+	int x, y,n, count = 1;
 	memset(a, 0, sizeof(a));
-	tot = a[x = 0][y = n - 1] = 1;
-	while (tot < n * n) {
-		while (x + 1 < n && !a[x + 1][y]) a[++x][y] = ++tot;
-		while (y - 1 >= 0 && !a[x][y-1]) a[x][--y] = ++tot;
-		while (x - 1 >=0 && !a[x-1][y]) a[--x][y] = ++tot;
-		while (y + 1 < n && !a[x][y+1]) a[x][++y] = ++tot;
+	scanf("%d", &n);
+	x = 0;
+	y = n - 1;
+	a[x][y] = 1;
+	while (count < n * n) {
+		while (x+1 < n && !a[x+1][y]) a[++x][y] = ++count;
+		while (y - 1 >= 0 && !a[x][y - 1]) a[x][--y] = ++count;
+		while (x - 1 >= 0 && !a[x - 1][y]) a[--x][y] = ++count;
+		while (y + 1 < n && !a[x][y + 1]) a[x][++y] = ++count;
 	}
-	for (x = 0; x < n; x++) {
-		for (y = 0; y < n; y++) {
-			printf("%3d", a[x][y]);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			printf("%3d", a[i][j]);
 		}
 		printf("\n");
 	}
-	return 0;
+
 }
